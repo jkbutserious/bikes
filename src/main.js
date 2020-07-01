16514417b97aacab8068c14ab72061f419f95421
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-01 15:21:27 -0700

    refactor api call to include promises
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- src/main.js
+++ src/main.js
@@ -9,18 +9,28 @@ let request = new XMLHttpRequest();
 
 $(document).ready(function() {
   $("#bikeInfo").click(function() {
-    const city = $("#location").val();
-    const url = `https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`;
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response)
+    let city = $("#location").val();
+    $("#location").val("");
+
+    let promise = new Promise(function(resolve, reject) {
+      const url = `https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(request.response);
+        }
       }
-    }
-    request.open("GET", url, true);
-    request.send();
-    const getElements = function (response) {
-      $("#stolenBikeCount").text(`Number of bikes stolen in your location: ${response.proximity}`);
-    }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $("#stolenBikeCount").text(`Number of bikes stolen in your location: ${body.proximity}`);
+    }, function(error) {
+      $("#stolenBikeCount").text(`There was an error processing your request: ${error}`);
+    })
   });
 });
