03788139f87c9986d5f7acc9ba9968f39a5403b9
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-01 15:47:26 -0700

    refactor api call to use fetch statement

--- src/main.js
+++ src/main.js
@@ -5,32 +5,59 @@ import './styles.css';
 
 
 // in .env API_KEY and SECRET
-let request = new XMLHttpRequest();
+// let request = new XMLHttpRequest();
 
+
+function getElements(response) {
+  if (response.proximity) {
+    $("#stolenBikeCount").text(`Total Bikes: ${response.proximity}`);
+    $('.showErrors').text("");
+  } else {
+    $("#stolenBikeCount").text("");
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
 $(document).ready(function() {
   $("#bikeInfo").click(function() {
     let city = $("#location").val();
     $("#location").val("");
-
-    let promise = new Promise(function(resolve, reject) {
-      const url = `https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`;
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        }
-        else {
-          reject(request.response);
-        }
+    
+    fetch(`https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`)
+    .then(function(response) {
+      console.log(response)
+      if (!response.ok) {
+        throw Error(response.statusText);
       }
-      request.open("GET", url, true);
-      request.send();
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
+    .then(function(jsonifiedResponse) {
+      console.log(jsonifiedResponse);
+      getElements(jsonifiedResponse);
     });
 
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $("#stolenBikeCount").text(`Number of bikes stolen in your location: ${body.proximity}`);
-    }, function(error) {
-      $("#stolenBikeCount").text(`There was an error processing your request: ${error}`);
-    })
+
+    // let promise = new Promise(function(resolve, reject) {
+    //   const url = `https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`;
+    //   request.onload = function () {
+    //     if (this.status === 200) {
+    //       resolve(request.response);
+    //     }
+    //     else {
+    //       reject(request.response);
+    //     }
+    //   }
+    //   request.open("GET", url, true);
+    //   request.send();
+    // });
+
+  //   promise.then(function(response) {
+  //     const body = JSON.parse(response);
+  //     $("#stolenBikeCount").text(`Number of bikes stolen in your location: ${body.proximity}`);
+  //   }, function(error) {
+  //     $("#stolenBikeCount").text(`There was an error processing your request: ${error}`);
+  //   })
   });
 });
